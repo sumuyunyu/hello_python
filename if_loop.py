3343bfae19e01531d3097bad45b5ee396f675e48
Jxy <sumuyunyu@gmail.com>
2018-10-15 21:58:30 +0800

    新添加了一个文件，第五章的代码

---
+++ if_loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/15 21:52
+# @Author  : jxyxyj@gmail.com
+# @Site    : 
+# @File    : if_loop.py
+# @Software: PyCharm
+x = 3
+print(x, "34343", ',', 3)
